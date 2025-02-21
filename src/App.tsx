555e5adf0a280ac12929d4cb4a144ea6a2c07db4
Ben2811 <luanchuong51@gmail.com>
2025-02-21 20:12:23 +0700

    merge Home & About, remove Law

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import NavBar from "./components/NavBar";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-import About from "./pages/About";
 import "./App.css";
 import Footer from "./components/Footer";
 import Banner from "./components/Banner";
@@ -10,8 +9,7 @@ import Member from "./pages/team/Member";
 import ContactInfo from "./pages/ContactInfo";
 import Activities from "./pages/activities/Activities";
 import Activity from "./pages/activities/Activity";
-import Law from "./pages/laws/Law";
-import Laws from "./pages/laws/Laws";
+
 import Procedures from "./pages/procedures/Procedures";
 import GeneralServices from "./pages/generalServices/GeneralServices";
 import GeneralService from "./pages/generalServices/GeneralService";
@@ -27,13 +25,10 @@ function App() {
       <hr />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
         <Route path="/activities" element={<Activities />} />
         <Route path="/activities/:id" element={<Activity/>}/>
         <Route path="/team" element={<Team />} />
         <Route path="/team/:id" element={<Member />} />
-        <Route path="/laws" element={<Laws />} />
-        <Route path="/laws/:id" element={<Law />} />
         <Route path="/procedures" element={<Procedures />} />
         <Route path="/services" element={<GeneralServices/>} />
         <Route path="/services/:id" element={<GeneralService/>}/>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,30 @@
 import "../App.css";
+import Carousel from "../components/Carousel";
+
+const images = ["./house.jpg", "./house.jpg"];
 
 const Home = () => {
   return (
-    <div className="flex flex-col gap-32 min-h-screen items-center">
+    <div className="flex flex-col gap-16 min-h-screen items-center text-center m-x-5">
       <div className="flex flex-col items-center text-center gap-4 mt-6 font-semibold">
         <h2> CHÀO MỪNG QUÝ KHÁCH TỚI</h2>
         <h1 className="font-bold">VĂN PHÒNG LUẬT SƯ TÙNG LÂM</h1>
         <div className="w-1/2 h-[10px] rounded-xl bg-[#0052A2] "></div>
+      </div>
+      <Carousel images={images} />
+      <div className="flex flex-col items-center text-center gap-4 mt-6">
+        <h2> 10 năm kinh nghiệm hành nghề</h2>
+        <h1 className="font-bold">VĂN PHÒNG LUẬT SƯ TÙNG LÂM</h1>
+        <div className="w-1/2 h-[10px] rounded-xl bg-[#0052A2] "></div>
+        <p>Văn phòng luật sư Tùng Lâm......</p>
+      </div>
+      <div className="flex flex-col justify-center items-center gap-4">
+        <h1>Long heading is what you see here in this feature section</h1>
+        <img
+          src="./groupimageDemo.jpg"
+          className="w-[600px] h-[400px] object-cover"
+          alt="Group image"
+        />
       </div>
       <div className="flex flex-row gap-4 justify-between">
         <div>images</div>
@@ -43,7 +61,9 @@ const Home = () => {
       </div>
       <div className="flex flex-col items-center text-center gap-4 font-bold">
         <h1>Short heading here</h1>
-        <button className="text-black bg-white border-2 border-[#064273] font-bold">ĐẶT LỊCH HẸN</button>
+        <button className="text-black bg-white border-2 border-[#064273] font-bold">
+          ĐẶT LỊCH HẸN
+        </button>
       </div>
     </div>
   );
